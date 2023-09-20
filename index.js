8c6585f1c085e2b5315b9956a8302303d74c8ae7
hellsgor <hellsgor@gmail.com>
2023-09-20 15:22:30 +0400

    add pluralize function

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = pluralize;
+
+function pluralize(startNumber, singular, plural, pluralGreaterThan4) {
+  const number = startNumber % 100;
+  let persuade;
+  if (number >= 11 && number <= 19) {
+    persuade = pluralGreaterThan4;
+  } else {
+    const lastNumber = startNumber % 10;
+    switch (lastNumber) {
+      case (1):
+        persuade = singular;
+        break;
+      case (2):
+      case (3):
+      case (4):
+        persuade = plural;
+        break;
+      default:
+        persuade = pluralGreaterThan4;
+    }
+  }
+  return `${startNumber} ${persuade} `;
+}
